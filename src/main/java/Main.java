c3614ecdd7c74ffe733347c3fa6238df1d72b761
hyeonee <hyoosscode@gmail.com>
2025-07-13 10:53:18 +0900

    클래스

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,7 @@
-import java.util.*;
-import java.lang.String;
-
-public class Main{
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int[][] scoreArray = {{85, 70, 90, 95}, {80, 95, 90, 75}, {75, 85, 90, 80}};
-        for(int i=0;i<3;i++){
-            int sum=0;
-            for(int j=0;j<4;j++){
-                sum += scoreArray[i][j];
-            }
-            System.out.println("학생"+(i+1)+":"+Arrays.toString(scoreArray[i])+"평균:"+((double)(sum/scoreArray[i].length)));
-
-        }
+public class Main {
+    public static void main(String[] args){
+        Car car=new Car();
+        System.out.println("");
+        Car mycar=new Car("gd",2025);
     }
 }

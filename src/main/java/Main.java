22a02b0c7858cb9c2d9b3cd20f8162713c3adf32
hyeonee <hyoosscode@gmail.com>
2025-07-13 11:27:05 +0900

    클래스

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
-public class Main {
+public class Main{
     public static void main(String[] args){
-        Car car=new Car();
-        System.out.println("");
-        Car mycar=new Car("gd",2025);
+        Person person=new Person("김길벗",21);
+        person.displayInfo();
+        person.setName("이코천");
+        person.setAge(22);
+        System.out.println("수정한 이름:"+person.getName());
+        System.out.println("수정한 나이:"+person.getAge());
     }
+
 }

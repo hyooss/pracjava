34882f73da0804ee7585bfb5609a529d9d2ed025
hyeonee <hyoosscode@gmail.com>
2025-07-13 14:18:03 +0900

    클래스

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        Person person=new Person("김길벗",21);
-        person.displayInfo();
-        person.setName("이코천");
-        person.setAge(22);
-        System.out.println("수정한 이름:"+person.getName());
-        System.out.println("수정한 나이:"+person.getAge());
+        Dog mydog=new Dog();
+        Cat mycat=new Cat();
     }
-
 }
